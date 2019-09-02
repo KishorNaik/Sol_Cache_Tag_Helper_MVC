5de997c3773376023da821572cbda1910b2a3d0f
Kishor Naik <kishor.naik011.net@gmail.com>
2019-09-02 13:45:07 +0530

    Code Commit

---
+++ Sol_Cache_Tag_Helper/Sol_Cache_Tag_Helper/Controllers/CacheDemoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// https://www.davepaquette.com/archive/2015/06/03/mvc-6-cache-tag-helper.aspx
+// https://codingblast.com/asp-net-core-mvc-caching-taghelpers/
+
+namespace Sol_Cache_Tag_Helper.Controllers
+{
+    public class CacheDemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [BindProperty(SupportsGet =true)]
+        public int id { get; set; }
+
+        [HttpGet("varbyroute/{id:int}")]
+        public IActionResult VarByRouteDemo()
+        {
+            return View();
+        }
+
+        [HttpPost("submit/{id:int}")]
+        public IActionResult VarByDemo()
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
---
+++ Sol_Cache_Tag_Helper/Sol_Cache_Tag_Helper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sol_Cache_Tag_Helper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
